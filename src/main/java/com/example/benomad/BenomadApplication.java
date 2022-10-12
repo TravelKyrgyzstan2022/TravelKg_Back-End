ca4a29d02a7b1d7e3ee00b3255f74f1b9e7494f6
unknown <Travelkyrgyzstan2022@gmail.com>
2022-10-12 11:41:48 +0600

    init

---
+++ src/main/java/com/example/benomad/BenomadApplication.java
@@ -0,0 +1,13 @@
+package com.example.benomad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenomadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BenomadApplication.class, args);
+	}
+
+}
