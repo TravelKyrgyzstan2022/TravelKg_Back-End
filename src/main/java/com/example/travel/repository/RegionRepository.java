47054d60d6587d1a078bc4ef517ec9cdece47a18
itsabdiev <73123509+itsabdiev@users.noreply.github.com>
2022-10-14 23:17:28 +0600

    Delete src/main/java/com/example/travel directory

--- src/main/java/com/example/travel/repository/RegionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.travel.repository;
-
-import com.example.travel.entity.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RegionRepository extends JpaRepository<Region,Long> {
-}
