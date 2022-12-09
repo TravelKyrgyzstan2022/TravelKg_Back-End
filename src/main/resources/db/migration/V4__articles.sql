0860040ec914af576774617a9b8626d14e5a74e5
itsabdiev <73123509+itsabdiev@users.noreply.github.com>
2022-12-09 19:46:13 +0600

    added articles

--- src/main/resources/db/migration/V4__articles.sql
+++ src/main/resources/db/migration/V4__articles.sql
@@ -1,37 +1,64 @@
-INSERT INTO articles (id, title, body,user_id) VALUES (nextval('articles_id_seq'), '5 reasons to visit Kyrgyzstan',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo ipsum sit amet augue sagittis mollis. Aenean consequat augue interdum felis commodo, vitae fermentum nisi suscipit. Ut tristique lacus ac imperdiet blandit. Suspendisse non massa auctor, placerat ligula sed, consectetur purus. Integer vel ex libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque facilisis nisi ut justo rhoncus, eu hendrerit urna ornare.
+INSERT INTO articles (id, title, body,user_id) VALUES (nextval('articles_id_seq'), '5 Reasons to Visit Kyrgyzstan',
+        '1)	The Landscape
+        Kyrgyzstan is a mountainous oasis in the desert steppes of Central Asia, enclosed by the celestial Tien-Shan Mountain ranges, with countless glaciers and lakes, from where water flows abundantly. It is not surprising that the country is popular amongst hikers. Many people call Kyrgyzstan as a “Second Switzerland” because of its wonderful and breathtaking scenery.
+        2)	Fascinating cultures and traditions
+        Kyrgyz culture is closely connected with nomadism in everyday life with holidays, crafts, music and cuisine. The nomadic life and livestock culture are based primarily on the animals and what tending and harvesting from them require during different seasons of the year. The harsh landscape and sparsely populated areas have grown a culture where quests were traditionally granted with the best of everything a host could provide and this generosity carries over to the modern-day travelers as well especially in the rural areas. For centuries Kyrgyz people have made many beautiful and fascinating things from sheep’s wool and animal skin.
+        3)    Kyrgyz Cuisine
+        Traditional Kyrgyz cuisine is a combination of ancient nomadic culture of the Kyrgyz people that borrows dishes and recipes from more than 80 cultures and nationalities living in Kyrgyzstan, as well as the neighbors, Kazakhstan, Uzbekistan and China. The lifestyle of the nomads included a constant movement from place to another alone the steppes and mountains both in cold and warm weather. Therefore, ancestors’ food was high in calories and very nutritious. These recipes have been passed down from generation to generation and are still a part of the Kyrgyz cuisine.
+        4)	Over 2000 spectacular lakes
+        In Kyrgyzstan, a country with untouched nature, there are about 2000 lakes, ranging from large to small. Each lake is unique and beautiful in its own way. Among the most popular lakes are Issyk-Kol – the pearl of Kyrgyzstan and Central Asia. Son-kol – it has a large selection of yurt camps. Sary Chelek – biosphere reserve in Jalal-Abad region, the flora and fauna of the reserve is very rich.
+        5)	Hospitality of the nation
+        The Kyrgyz people have been famous since ancient times for their openness, kindness, respect and hospitability. When it comes to the question “What is the main attraction of Kyrgyzstan?” it is not a building, monument or a specific place that comes to mind, but the legendary Kyrgyz hospitality. After all, it is in the blood of the Kyrgyz. Do not be surprised if during a trip through Kyrgyzstan, especially in rural areas, local people invite you for tea and treat you with different delicacies.
+        ',1);
+INSERT INTO articles (id, title, body,user_id) VALUES (nextval('articles_id_seq'), 'Fun Activities everyone should try while in Kyrgyzstan',
+        '·       Sleep in a yurt
+        Yurts are the epitome of nomadic life and no trip to Kyrgyzstan would be complete without spending at least one night amidst the cosy felt surroundings of a traditional yurt.
 
-Ut rhoncus ultricies ligula, id pulvinar erat. Pellentesque ultricies porttitor nulla sit amet facilisis. Donec in erat sed lacus eleifend auctor at ac odio. Morbi quis venenatis urna. Integer sit amet euismod purus. Mauris cursus urna velit, non venenatis leo pretium ut. Quisque massa felis, consequat id imperdiet quis, feugiat eu arcu. Sed lobortis vitae augue vel cursus. Suspendisse pulvinar ac urna elementum faucibus. Sed erat nisl, hendrerit quis congue at, mollis vel libero. Fusce ultrices ante non varius mattis. Fusce eu neque eu quam viverra blandit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas et eros tellus. Fusce arcu sem, commodo euismod turpis ac, fermentum fringilla ligula.
+        · 	Watch a Salbaruun
+        This is the name for traditional eagle hunting in Kyrgyzstan. Salbaruun is still very much a living tradition and eagles are trained throughout the country to chase and hunt prey.
 
-Donec rhoncus ac erat ac accumsan. Suspendisse quis lacus tortor. Donec finibus ultrices auctor. Suspendisse fermentum ligula quis nisl posuere maximus. Donec eget felis ac dui tristique ullamcorper eget eget ipsum. Suspendisse porta varius turpis, eget maximus mauris pharetra sit amet. Suspendisse sit amet risus augue. Cras finibus lacus eu ipsum volutpat blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vitae suscipit elit, id aliquet quam. Morbi congue elementum pellentesque. Suspendisse nisl mauris, rutrum quis risus sed, porta varius nibh. Morbi molestie, libero ut dictum sollicitudin, elit ante dapibus erat, nec egestas nibh nibh vel lorem. Phasellus sodales metus vitae tempor porttitor. Suspendisse ut ipsum non nunc fermentum tristique.
+        · 	Visit the Magical Fairytale Canyon
+        Always wanted to see what it’s like on Mars? A trip to Skazka canyon on the south shore of Lake Issyk-Kul will have you feeling pretty close. The incredible valley of red sandstone rocks shaped by the elements in all kinds of hills and valleys.
+        · 	Watch Kok-Boru
+        Kok-boru is the national sport of Kyrgyzstan and is quite accurately described by tourists as ‘dead goat polo’. Two teams on horse back wrestle over a headless dead goat carcass and try to get it in the pit at either end to score a point. Sound brutal? It is!
+        · 	Go Horse Ride
+        Horse riding is one of the interesting and fun way of exploring the landscape of Kyrgyzstan. Horse trekking is very famous attraction in almost all regions, there will not be any problem to find a place for horse riding.
+        · 	Try National Drinks like Kumyz
+        Kumyz is fermented horse milk and tastes about as good as it sounds… Kyrgyz drink it regularly and it is often served as a welcome drink for guests.
+        ',1);
+INSERT INTO articles (id, title, body,user_id) VALUES (nextval('articles_id_seq'), 'Transportation system in Kyrgyzstan - handy lifehacks',
+        'The most suitable way to travel around the country is by bus, taxis, and taxis. You can ride them both in large cities and between cities. Often, this is the only way to get to the right place.
+        The bus fleet of Bishkek was updated in 2009 and today is represented by quite comfortable buses.
+        Taxis operate in all cities and towns of Kyrgyzstan. Tariffs are initially very low, it is recommended to call a corporate taxi, and not a private trader (more comfortable and safer).
+        By the wat road network is poorly developed, good asphalt roads go from Bishkek to Osh, Almaty, and Balykchy, and a good asphalt road and the road to Torugart in the direction of China are also laid around Issyk-Kul Lake. In winter, the movement is hampered by drifts on the passes. The roads are either broken asphalt or graveled or just unpaved roads. However, mountain roads are usually specially converted private trucks (crews) drive.
+        Many places can only be reached by helicopter, on horseback or on foot.
+        AVIATION
+        International airport “Manas” is located near the capital of Kyrgyzstan – Bishkek. Domestic flights to Osh, Batken, and Jalal-Abad are operated from Bishkek.
+        Main part of the country’s fleet is the old, leased Boeing-737 or remaining from the time of the USSR – airplanes.
+        Air Bishkek (www.air.kg)
+        Air Kyrgyzstan (www.air.kg)
+        Avia Traffic (www.aero.kg)
+        Sky Bishkek (https://skybishkek.kg)
+        RAILWAY TRANSPORT
+        Passenger train message was kept only in the north of the country, near Bishkek. From there, there are three long-distance trains to Moscow, Ekaterinburg, and Novokuznetsk, and there is a suburban connection to the west, to Kazakhstan (in the direction of Merke, before traveling 3639 km), to the east (to Tokmak station) and a local train to Rybachye (Balykchy) on the lake Issyk-Kul.
+        WATER CARRIAGE
+        Since there are very few navigable rivers in the country, only small private boats and boats are present. On the Issyk-Kul lake runs the ship.
+        CAR RENTAL
+        As a rule, rental cars are provided without a driver. At the request of the tourist can rent a car with a driver.
+        To get a car for rent you will need the following documents:
+        foreigners need a passport and a document confirming the legality of their stay in the country;
+        International driver’s license.
+        ', 1);
 
-Vivamus sed lectus eget dui elementum luctus posuere quis nisi. Pellentesque vitae consectetur dui. Fusce eleifend nisi id interdum laoreet. Etiam pulvinar consequat ex at posuere. Quisque venenatis libero nibh, a sollicitudin elit varius in. Fusce non pellentesque dolor. Curabitur feugiat metus vitae consectetur tempor. Aenean aliquet ante aliquet venenatis imperdiet. Vestibulum condimentum lectus metus, vitae finibus nulla aliquam non. Aliquam ac dui dui. Sed finibus ullamcorper sagittis. Morbi quis interdum risus. Quisque quis iaculis dui. Aenean vehicula sagittis nunc, nec ultricies eros luctus ut. Sed ac volutpat enim, id dapibus lorem. Nam interdum ipsum ipsum, et condimentum lacus eleifend ac.
+INSERT INTO article_image_urls VALUES (1,'https://i.ibb.co/1G56yCY/article1.jpg');
+INSERT INTO article_image_urls VALUES (1,'https://i.ibb.co/0ts5ZJX/article31.jpg');
+INSERT INTO article_image_urls VALUES (1,'https://i.ibb.co/2F59dzt/article32.jpg');
+INSERT INTO article_image_urls VALUES (2,'https://i.ibb.co/0fMFH3C/article21.jpg');
+INSERT INTO article_image_urls VALUES (2,'https://i.ibb.co/d4GTXQR/article22.jpg');
+INSERT INTO article_image_urls VALUES (2,'https://i.ibb.co/ZhXcrr0/article25.jpg');
+INSERT INTO article_image_urls VALUES (2,'https://i.ibb.co/Vgmp0NG/article24.jpg');
+INSERT INTO article_image_urls VALUES (3,'https://avatars.mds.yandex.net/get-altay/2425845/2a00000176f03468f3c7f012da2d057fe6e9/XXL');
+INSERT INTO article_image_urls VALUES (3,'https://images.unsplash.com/photo-1569629743817-70d8db6c323b?ixlib=rb-4.0.3&w=1080&fit=max&q=80&fm=jpg&crop=entropy&cs=tinysrgb');
+INSERT INTO article_image_urls VALUES (3,'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2019_13/2798361/190325-rental-cars-cs-229p.jpg');
 
-Ut posuere, mauris vitae luctus hendrerit, nisl odio suscipit dui, non convallis purus nunc fermentum eros. Quisque tincidunt velit vel tellus consectetur, et iaculis mauris aliquam. Mauris pulvinar ac tortor vitae lobortis. Mauris vel leo fringilla, posuere justo sit amet, tincidunt metus. Morbi eu elit eleifend ante aliquam tempus. Fusce et lectus tincidunt, molestie risus sed, sollicitudin ex. Mauris gravida ornare nunc id blandit. Nunc placerat, augue ac fringilla luctus, eros lectus posuere enim, at commodo dui ex a velit. Ut elementum aliquet sapien. Aliquam ex felis, ullamcorper ut dui eget, ullamcorper dignissim elit.',
-        1);
-INSERT INTO articles (id, title, body,user_id) VALUES (nextval('articles_id_seq'), 'Issyk-Kul Lake - All You Need To Know',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo ipsum sit amet augue sagittis mollis. Aenean consequat augue interdum felis commodo, vitae fermentum nisi suscipit. Ut tristique lacus ac imperdiet blandit. Suspendisse non massa auctor, placerat ligula sed, consectetur purus. Integer vel ex libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque facilisis nisi ut justo rhoncus, eu hendrerit urna ornare.
-
-Ut rhoncus ultricies ligula, id pulvinar erat. Pellentesque ultricies porttitor nulla sit amet facilisis. Donec in erat sed lacus eleifend auctor at ac odio. Morbi quis venenatis urna. Integer sit amet euismod purus. Mauris cursus urna velit, non venenatis leo pretium ut. Quisque massa felis, consequat id imperdiet quis, feugiat eu arcu. Sed lobortis vitae augue vel cursus. Suspendisse pulvinar ac urna elementum faucibus. Sed erat nisl, hendrerit quis congue at, mollis vel libero. Fusce ultrices ante non varius mattis. Fusce eu neque eu quam viverra blandit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas et eros tellus. Fusce arcu sem, commodo euismod turpis ac, fermentum fringilla ligula.
-
-Donec rhoncus ac erat ac accumsan. Suspendisse quis lacus tortor. Donec finibus ultrices auctor. Suspendisse fermentum ligula quis nisl posuere maximus. Donec eget felis ac dui tristique ullamcorper eget eget ipsum. Suspendisse porta varius turpis, eget maximus mauris pharetra sit amet. Suspendisse sit amet risus augue. Cras finibus lacus eu ipsum volutpat blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vitae suscipit elit, id aliquet quam. Morbi congue elementum pellentesque. Suspendisse nisl mauris, rutrum quis risus sed, porta varius nibh. Morbi molestie, libero ut dictum sollicitudin, elit ante dapibus erat, nec egestas nibh nibh vel lorem. Phasellus sodales metus vitae tempor porttitor. Suspendisse ut ipsum non nunc fermentum tristique.
-
-Vivamus sed lectus eget dui elementum luctus posuere quis nisi. Pellentesque vitae consectetur dui. Fusce eleifend nisi id interdum laoreet. Etiam pulvinar consequat ex at posuere. Quisque venenatis libero nibh, a sollicitudin elit varius in. Fusce non pellentesque dolor. Curabitur feugiat metus vitae consectetur tempor. Aenean aliquet ante aliquet venenatis imperdiet. Vestibulum condimentum lectus metus, vitae finibus nulla aliquam non. Aliquam ac dui dui. Sed finibus ullamcorper sagittis. Morbi quis interdum risus. Quisque quis iaculis dui. Aenean vehicula sagittis nunc, nec ultricies eros luctus ut. Sed ac volutpat enim, id dapibus lorem. Nam interdum ipsum ipsum, et condimentum lacus eleifend ac.
-
-Ut posuere, mauris vitae luctus hendrerit, nisl odio suscipit dui, non convallis purus nunc fermentum eros. Quisque tincidunt velit vel tellus consectetur, et iaculis mauris aliquam. Mauris pulvinar ac tortor vitae lobortis. Mauris vel leo fringilla, posuere justo sit amet, tincidunt metus. Morbi eu elit eleifend ante aliquam tempus. Fusce et lectus tincidunt, molestie risus sed, sollicitudin ex. Mauris gravida ornare nunc id blandit. Nunc placerat, augue ac fringilla luctus, eros lectus posuere enim, at commodo dui ex a velit. Ut elementum aliquet sapien. Aliquam ex felis, ullamcorper ut dui eget, ullamcorper dignissim elit.',
-        1);
-INSERT INTO articles (id, title, body,user_id) VALUES (nextval('articles_id_seq'), 'Топ 10 лучших мест в БИШКЕКЕ!',
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. In commodo ipsum sit amet augue sagittis mollis. Aenean consequat augue interdum felis commodo, vitae fermentum nisi suscipit. Ut tristique lacus ac imperdiet blandit. Suspendisse non massa auctor, placerat ligula sed, consectetur purus. Integer vel ex libero. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Pellentesque facilisis nisi ut justo rhoncus, eu hendrerit urna ornare.
-
-Ut rhoncus ultricies ligula, id pulvinar erat. Pellentesque ultricies porttitor nulla sit amet facilisis. Donec in erat sed lacus eleifend auctor at ac odio. Morbi quis venenatis urna. Integer sit amet euismod purus. Mauris cursus urna velit, non venenatis leo pretium ut. Quisque massa felis, consequat id imperdiet quis, feugiat eu arcu. Sed lobortis vitae augue vel cursus. Suspendisse pulvinar ac urna elementum faucibus. Sed erat nisl, hendrerit quis congue at, mollis vel libero. Fusce ultrices ante non varius mattis. Fusce eu neque eu quam viverra blandit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas et eros tellus. Fusce arcu sem, commodo euismod turpis ac, fermentum fringilla ligula.
-
-Donec rhoncus ac erat ac accumsan. Suspendisse quis lacus tortor. Donec finibus ultrices auctor. Suspendisse fermentum ligula quis nisl posuere maximus. Donec eget felis ac dui tristique ullamcorper eget eget ipsum. Suspendisse porta varius turpis, eget maximus mauris pharetra sit amet. Suspendisse sit amet risus augue. Cras finibus lacus eu ipsum volutpat blandit. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vitae suscipit elit, id aliquet quam. Morbi congue elementum pellentesque. Suspendisse nisl mauris, rutrum quis risus sed, porta varius nibh. Morbi molestie, libero ut dictum sollicitudin, elit ante dapibus erat, nec egestas nibh nibh vel lorem. Phasellus sodales metus vitae tempor porttitor. Suspendisse ut ipsum non nunc fermentum tristique.
-
-Vivamus sed lectus eget dui elementum luctus posuere quis nisi. Pellentesque vitae consectetur dui. Fusce eleifend nisi id interdum laoreet. Etiam pulvinar consequat ex at posuere. Quisque venenatis libero nibh, a sollicitudin elit varius in. Fusce non pellentesque dolor. Curabitur feugiat metus vitae consectetur tempor. Aenean aliquet ante aliquet venenatis imperdiet. Vestibulum condimentum lectus metus, vitae finibus nulla aliquam non. Aliquam ac dui dui. Sed finibus ullamcorper sagittis. Morbi quis interdum risus. Quisque quis iaculis dui. Aenean vehicula sagittis nunc, nec ultricies eros luctus ut. Sed ac volutpat enim, id dapibus lorem. Nam interdum ipsum ipsum, et condimentum lacus eleifend ac.
-
-Ut posuere, mauris vitae luctus hendrerit, nisl odio suscipit dui, non convallis purus nunc fermentum eros. Quisque tincidunt velit vel tellus consectetur, et iaculis mauris aliquam. Mauris pulvinar ac tortor vitae lobortis. Mauris vel leo fringilla, posuere justo sit amet, tincidunt metus. Morbi eu elit eleifend ante aliquam tempus. Fusce et lectus tincidunt, molestie risus sed, sollicitudin ex. Mauris gravida ornare nunc id blandit. Nunc placerat, augue ac fringilla luctus, eros lectus posuere enim, at commodo dui ex a velit. Ut elementum aliquet sapien. Aliquam ex felis, ullamcorper ut dui eget, ullamcorper dignissim elit.',
-        1);
-INSERT INTO article_image_urls VALUES (1,'https://oir.mobi/uploads/posts/2021-05/thumbs/1622302143_13-oir_mobi-p-priroda-kirgizstana-priroda-krasivo-foto-13.jpg');
-INSERT INTO article_image_urls VALUES (2,'https://concept.kg/media/redactor/ik_DE2wlxk.jpg');
-INSERT INTO article_image_urls VALUES (3,'https://tripplanet.ru/wp-content/uploads/asia/kyrgyzstan/bishkek/dostoprimechatelnosti-bishkeka.jpg');
 
