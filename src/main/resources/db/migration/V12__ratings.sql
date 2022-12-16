5d60a1b13a3eef00e85ca2552b3482943b18b2ea
TravelKyrgyzstan2022 <115484543+TravelKyrgyzstan2022@users.noreply.github.com>
2022-12-17 01:57:45 +0600

    added ratings slq

---
+++ src/main/resources/db/migration/V12__ratings.sql
@@ -0,0 +1,16 @@
+update places set description='Ideal for family holidays. Preserving and honoring the centuries-old traditions of oriental hospitality, family values and respect for elders' where id = 33;
+insert into place_ratings values (nextval('place_ratings_id_seq'),5,20,5);
+insert into place_ratings values (nextval('place_ratings_id_seq'),5,20,6);
+insert into place_ratings values (nextval('place_ratings_id_seq'),5,24,7);
+insert into place_ratings values (nextval('place_ratings_id_seq'),5,24,8);
+insert into place_ratings values (nextval('place_ratings_id_seq'),5,26,10);
+insert into place_ratings values (nextval('place_ratings_id_seq'),4,26,9);
+insert into place_ratings values (nextval('place_ratings_id_seq'),5,15,11);
+insert into place_ratings values (nextval('place_ratings_id_seq'),4,15,12);
+insert into place_ratings values (nextval('place_ratings_id_seq'),4,16,5);
+insert into place_ratings values (nextval('place_ratings_id_seq'),4,16,7);
+
+
+
+
+
