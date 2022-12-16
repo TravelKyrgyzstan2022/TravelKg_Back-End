12cc2dc8923843dc8e31e1f7b6171f2f7d2d8adc
TravelKyrgyzstan2022 <115484543+TravelKyrgyzstan2022@users.noreply.github.com>
2022-12-17 03:49:08 +0600

    added 4rd article

---
+++ src/main/resources/db/migration/V13__artilces2.sql
@@ -0,0 +1,9 @@
+INSERT INTO articles (id, title, body,user_id) VALUES (nextval('articles_id_seq'), 'Fascinating Facts about Kyrgyzstan',
+'       * The country''s capital, Bishkek, is its largest city. The population here in 2019 exceeded one million people.
+        * Among all the CIS countries, Kyrgyzstan occupies the 7th place in terms of area.
+        * It is Kyrgyzstan, most likely, that is the birthplace of walnuts.
+        * Bishkek was at one time called "Frunze" during the Soviet era. At the same time, the Soviet authorities did not take into account that there is no “f” sound in the Kyrgyz language, and the city was called “Prunze”.
+        * Once upon a time, the path of the famous Alexander the Great ran through the territory of modern Kyrgyzstan (interesting facts about Alexander the Great).
+        * The Kyrgyz language is the state language in Kyrgyzstan, but Russian also has official status.
+        * Astronauts claim that from near-Earth orbit, the famous Kyrgyz lake Issyk-Kul resembles a human eye.',1);
+INSERT INTO article_image_urls VALUES (4,'https://mobimg.b-cdn.net/v3/fetch/b6/b649d0bb075ebe12bfcaee3d7cbe2a92.jpeg');
