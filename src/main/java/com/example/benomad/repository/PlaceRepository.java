0efce4355f3c341a8941de0d48ab518622b6426c
TravelKyrgyzstan2022 <115484543+TravelKyrgyzstan2022@users.noreply.github.com>
2022-12-17 02:03:27 +0600

    Update PlaceRepository.java

--- src/main/java/com/example/benomad/repository/PlaceRepository.java
+++ src/main/java/com/example/benomad/repository/PlaceRepository.java
@@ -17,4 +17,10 @@ public interface PlaceRepository extends JpaRepository<Place, Long> {
     @Query(value = "select * from places p where p.place_category in :categories and p.place_type in :types and p.region in :regions", nativeQuery = true,
             countQuery ="select count(*) from places p where p.place_category in :categories and p.place_type in :types and p.region in :regions")
     Page<Place> findPlacesByPlaceCategoriesAndPlaceTypes(@Param("categories") List<String> categories, @Param("types") List<String> types,@Param("regions") List<String> regions,Pageable pageable);
+    
+    @Query(value = "" +
+            "SELECT p.* FROM places p JOIN " +
+            "(SELECT t.place_id AS place_id, AVG(t.rating) AS avg_rating FROM place_ratings t GROUP BY t.place_id) r " +
+            "ON p.id = r.place_id WHERE p.place_category in :categories ORDER BY r.avg_rating DESC LIMIT :limit",nativeQuery = true)
+    List<Place>  findTop5PlacesForPlaceCategory(@Param("categories") List<String> categories,@Param("limit") Integer limit);
 }
